2680649249f16ab79a66dcfb6e116a0b774d3486
Rishabh Sharma <rishabh.sharma9102@gmail.com>
2019-01-21 19:16:04 -0800

    Transformed HTML into Aggiefeed post structure. Also  tested and added temperature recieving method which is used to display temp on post.

--- WeatherScript.js
+++ WeatherScript.js
@@ -1,6 +1,6 @@
 // module is similar to main() in other languages. it takes in its name and dependancies as param
 var app = angular.module('weatherApp', []);
-app.controller('weatherCantroller', function ($scope, $http) {
+app.controller('weatherController', function ($scope, $http) {
 
     $scope.weatherResult = {};  // Weather details about Davis
     $scope.weatherResult2 = {};  // Weather details aobut Sacramento
@@ -29,4 +29,50 @@ app.controller('weatherCantroller', function ($scope, $http) {
             $scope.ErrorResult = response;
         });
     }
+
+    // WeatherService for Sacramento
+    $scope.weatherService2 = function () {
+        $http({
+            method: 'POST',
+            url: 'http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q=Sacramento'
+        }).then(function (response) {
+            $scope.weatherResult2.cityName = response.data.name;
+            $scope.weatherResult2.condition = response.data.weather[0].main;
+            $scope.weatherResult2.Description = response.data.weather[0].description;
+            $scope.weatherResult2.IconUrl = "http://openweathermap.org/img/w/" + response.data.weather[0].icon + ".png";
+            $scope.weatherResult2.Wind = response.data.wind.speed;
+            $scope.weatherResult2.Gusts = response.data.wind.gust;
+            $scope.weatherResult2.Humidity = response.data.main.humidity;
+            $scope.GetTemperature2(response.data.main);
+
+        }, function errorCallback(response) {
+            $scope.ErrorResult = response;
+        });
+    }
+
+
+    // Get temp for the Davis
+    $scope.GetTemperature = function (temps) {
+
+        var FCurrent = $scope.GetFarenheit(temps.temp).toFixed(1);  // toFixed to fix to 1 decimal place
+        var FLow = $scope.GetFarenheit(temps.temp_min).toFixed(1);
+        var FHigh = $scope.GetFarenheit(temps.temp_max).toFixed(1);
+
+        $scope.FTemps = [FCurrent, FLow, FHigh];
+
+        var CCurrent = $scope.GetCelsius(temps.temp).toFixed(1);
+        var CLow = $scope.GetCelsius(temps.temp_min).toFixed(1);
+        var CHigh = $scope.GetCelsius(temps.temp_max).toFixed(1);
+
+        $scope.CTemps = [CCurrent, CLow, CHigh];
+
+        var KCurrent = $scope.GetKelvin(temps.temp).toFixed(1);
+        var KLow = $scope.GetKelvin(temps.temp_min).toFixed(1);
+        var KHigh = $scope.GetKelvin(temps.temp_max).toFixed(1);
+
+        $scope.KTemps = [KCurrent, KLow, KHigh];
+
+    }
+
+
 });
